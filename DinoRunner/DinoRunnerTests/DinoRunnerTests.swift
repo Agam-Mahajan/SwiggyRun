e82e00df7f789dc4e0e9f40122c6c7d0e504b366
Agam Mahajan <agam.mahajan@swiggy.in>
2022-08-31 16:57:10 +0530

    Add unit Test

---
+++ DinoRunner/DinoRunnerTests/DinoRunnerTests.swift
@@ -0,0 +1,33 @@
+//
+//  DinoRunnerTests.swift
+//  DinoRunnerTests
+//
+//  Created by Palak Vij on 31/08/22.
+//  Copyright © 2022 John Kuhn. All rights reserved.
+//
+
+import XCTest
+
+class DinoRunnerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(95, 95, "Scores are equal")
+    }
+    
+    func testExample2() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertNotEqual(95, 90, "Scores are unequal")
+    }
+
+}
